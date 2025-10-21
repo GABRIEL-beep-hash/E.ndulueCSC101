8330a7a230b6eb41f56abeccef71b544f56de682
GABRIEL-beep-hash <chukwuemekalim.uche-ndulue@pau.edu.ng>
2025-10-22 11:42:06 +1300

    (My seventh commit)

---
+++ .rs/week-3/practice_7/src/main.rs
@@ -0,0 +1,7 @@
+fn main() {
+    let float_with_separator = 11_000.555_001;  // Use underscores as separators
+    println!("float value {}", float_with_separator);
+
+    let int_with_separator = 50_000;  // Use underscores for integers
+    println!("int value {}", int_with_separator);
+}
