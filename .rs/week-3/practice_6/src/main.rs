290ed523f84a512893069c8d38747ba2c07f2244
GABRIEL-beep-hash <chukwuemekalim.uche-ndulue@pau.edu.ng>
2025-10-22 11:32:29 +1300

    (My sixth commit)

---
+++ .rs/week-3/practice_6/src/main.rs
@@ -0,0 +1,5 @@
+fn main() {
+    let interest: f32 = 8.0;  // Use float literal or cast
+    // OR: let interest: f32 = 8 as f32;
+    println!("Interest is {}", interest);
+}
---
+++ .rs/week-3/practice_6/target/debug/practice_6.d
@@ -0,0 +1,1 @@
+C:\Users\USER\OneDrive\Documents\E.ndulueCSC101\.rs\week-3\practice_6\target\debug\practice_6.exe: C:\Users\USER\OneDrive\Documents\E.ndulueCSC101\.rs\week-3\practice_6\src\main.rs
