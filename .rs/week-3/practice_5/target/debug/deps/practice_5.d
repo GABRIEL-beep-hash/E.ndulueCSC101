480d28e459dc3e8de63f589a7b63dad7bdd6c8ff
GABRIEL-beep-hash <chukwuemekalim.uche-ndulue@pau.edu.ng>
2025-10-22 11:14:26 +1300

    (My firth commit)

---
+++ .rs/week-3/practice_5/target/debug/deps/practice_5.d
@@ -0,0 +1,5 @@
+C:\Users\USER\OneDrive\Documents\E.ndulueCSC101\.rs\week-3\practice_5\target\debug\deps\practice_5.d: src\main.rs
+
+C:\Users\USER\OneDrive\Documents\E.ndulueCSC101\.rs\week-3\practice_5\target\debug\deps\practice_5.exe: src\main.rs
+
+src\main.rs:
