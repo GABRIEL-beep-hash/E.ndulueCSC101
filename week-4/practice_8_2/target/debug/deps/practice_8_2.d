02bbe5d08c1c80c352123f7cde383d2d25681c2d
GABRIEL-beep-hash <chukwuemekalim.uche-ndulue@pau.edu.ng>
2025-10-23 07:53:37 +1300

    (My practice_8_2 week-4 commit)

---
+++ week-4/practice_8_2/target/debug/deps/practice_8_2.d
@@ -0,0 +1,5 @@
+C:\Users\USER\OneDrive\Documents\E.ndulueCSC101\week-4\practice_8_2\target\debug\deps\practice_8_2.d: src\main.rs
+
+C:\Users\USER\OneDrive\Documents\E.ndulueCSC101\week-4\practice_8_2\target\debug\deps\practice_8_2.exe: src\main.rs
+
+src\main.rs:
---
+++ week-4/practice_8_2/target/debug/practice_8_2.d
@@ -0,0 +1,1 @@
+C:\Users\USER\OneDrive\Documents\E.ndulueCSC101\week-4\practice_8_2\target\debug\practice_8_2.exe: C:\Users\USER\OneDrive\Documents\E.ndulueCSC101\week-4\practice_8_2\src\main.rs
