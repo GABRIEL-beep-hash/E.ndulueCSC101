2376036bb11975f9c182d4223aad1670e7180168
GABRIEL-beep-hash <chukwuemekalim.uche-ndulue@pau.edu.ng>
2025-11-06 01:14:28 +1300

    (My practice_3 week-6)

--- week_6/practice_3/src/main.rs
+++ week_6/practice_3/src/main.rs
@@ -1,9 +1,9 @@
 fn main() {
-    let name1 = "Ayomide Adesokan";
+    let name1 = "uche-ndulue emeka";
     println!("My name is {}", name1);
 
     // find and replace
-    let name2 = name1.replace("Ayomide", "Adebare");
+    let name2 = name1.replace("gabriel", "mich");
     println!("You can also call me {}", name2);
     
     let faculty = "Faculty of Science and Technology";
---
+++ week_6/practice_3/target/debug/practice_3.d
@@ -0,0 +1,1 @@
+C:\Users\USER\OneDrive\Documents\E.ndulueCSC101\week_6\practice_3\target\debug\practice_3.exe: C:\Users\USER\OneDrive\Documents\E.ndulueCSC101\week_6\practice_3\src\main.rs
