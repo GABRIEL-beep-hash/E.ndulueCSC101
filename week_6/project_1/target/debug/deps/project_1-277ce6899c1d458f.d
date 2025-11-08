c15772c21bfa653b772033be3268ec08ea130c34
GABRIEL-beep-hash <chukwuemekalim.uche-ndulue@pau.edu.ng>
2025-11-09 00:06:08 +1300

    (My project_1 week-6)

---
+++ week_6/project_1/target/debug/deps/project_1-277ce6899c1d458f.d
@@ -0,0 +1,5 @@
+C:\Users\USER\OneDrive\Documents\E.ndulueCOS101\week_6\project_1\target\debug\deps\project_1-277ce6899c1d458f.d: src\main.rs
+
+C:\Users\USER\OneDrive\Documents\E.ndulueCOS101\week_6\project_1\target\debug\deps\libproject_1-277ce6899c1d458f.rmeta: src\main.rs
+
+src\main.rs:
